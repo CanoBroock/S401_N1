2037b176b56d4391136b5dd10b92a518cb21593e
CanoBroock <canobroock@gmail.com>
2022-11-08 18:45:44 +0100

    correcion

---
+++ src/main/java/Controllers/HelloWorldController.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+public class HelloWorldController {
+
+	public String saluda(String nom) {
+		String frase = "";
+
+		return frase = "Hola, " + nom + ". Estàs executant un projecte Gradle";
+	}
+
+	public String saluda2(String nom) {
+		String frase = "";
+
+		return frase = "Hola, " + nom + ". Estàs executant un projecte Gradle";
+	}
+
+}
