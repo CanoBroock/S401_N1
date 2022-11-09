74035608c5d49b1f12c593cdbff2c9b19a4d3c27
CanoBroock <canobroock@gmail.com>
2022-11-09 17:20:25 +0100

    .

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controller/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controller/HelloWorldController.java
@@ -1,20 +1,25 @@
 package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/service")
 public class HelloWorldController {
 
+	@GetMapping("/saluda")
 	public String saluda(String nom) {
 		String frase = "";
 
 		return frase = "Hola, " + nom + ". Estàs executant un projecte Gradle";
 	}
 
+	@GetMapping("/saluda2")
 	public String saluda2(String nom) {
 		String frase = "";
 
-		return frase = "Hola, " + nom + ". Estàs executant un projecte Gradle";
+		return frase = "Hola, " + nom + ". Estàs executant un projecte Maven";
 	}
 
 }
