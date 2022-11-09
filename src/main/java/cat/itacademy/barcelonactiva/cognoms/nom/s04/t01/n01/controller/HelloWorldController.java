a907849cc34a2b593e99373f3e7035562a8110ca
CanoBroock <canobroock@gmail.com>
2022-11-09 17:02:35 +0100

    correcion

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controller/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
-package Controllers;
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
 
 	public String saluda(String nom) {
