12858db73c2a2e68db53b32719ae60f67a5440c2
CanoBroock <canobroock@gmail.com>
2022-11-15 10:02:10 +0100

    correcion

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controller/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controller/HelloWorldController.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	@GetMapping("/HelloWorld")
-	public String saluda(@RequestParam(defaultValue = "UNKNOWN") String nom) {
-		return "Hola, " + nom + ". Estàs executant un projecte Maven";
-	}
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(defaultValue = "UNKNOWN") String nom) {
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
 
-	@RequestMapping(value = { "/HelloWorld2", "/HelloWorld2/{nom}" })
-	public String saluda2(@PathVariable(required = false) String nom) {
-
-		return "Hola, " + nom + ". Estàs executant un projecte Maven";
-	}
+    @RequestMapping(value = {"/HelloWorld2", "/HelloWorld2/{nom}"})
+    public String saluda2(@PathVariable(required = false) String nom) {
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
 }
